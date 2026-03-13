1a8395d6ce1ed7299cc9b907e4fb5f60324afe7a
VLAJ <139784105+VL-MR@users.noreply.github.com>
2026-03-13 18:31:23 +0300

    Update 20260313132322_SeedAdminUser.cs

--- SoundWave/Migrations/20260313132322_SeedAdminUser.cs
+++ SoundWave/Migrations/20260313132322_SeedAdminUser.cs
@@ -11,10 +11,20 @@ namespace CourseProject.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-            IF NOT EXISTS (SELECT 1 FROM Users WHERE Email='admin@admin.com')
-            INSERT INTO Users (Name, Email, Password)
-            VALUES ('admin','admin@admin.com','admin')
-            ");
+    IF NOT EXISTS (SELECT 1 FROM Users WHERE Email='admin@admin.com')
+    BEGIN
+        INSERT INTO Users (Name, Email, Password)
+        VALUES ('admin','admin@admin.com','admin')
+    END
+    
+    DECLARE @adminId INT = (SELECT TOP 1 UserID FROM Users WHERE Email='admin@admin.com')
+    
+    IF NOT EXISTS (SELECT 1 FROM Playlists WHERE UserID=@adminId AND PlaylistName='Favorites')
+    BEGIN
+        INSERT INTO Playlists (PlaylistName, PlaylistImage, UserID)
+        VALUES ('Favorites','default.png',@adminId)
+    END
+");
         }
 
         /// <inheritdoc />
